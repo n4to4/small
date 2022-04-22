566bfbadc1e723c484f49ba99536f2c85291925c
n4to4 <n4to4k@gmail.com>
2022-04-22 20:36:38 +0900

    tracing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod sample;
 
 use argh::FromArgs;
+use std::alloc::{GlobalAlloc, System};
 
 #[derive(FromArgs)]
 /// Small string demo
@@ -20,6 +21,26 @@ impl Subcommand {
         match self {
             Subcommand::Sample(x) => x.run(),
         }
+    }
+}
+
+pub struct Tracing {
+    pub inner: System,
+}
+
+impl Tracing {
+    pub const fn new() -> Self {
+        Self { inner: System }
+    }
+}
+
+unsafe impl GlobalAlloc for Tracing {
+    unsafe fn alloc(&self, layout: std::alloc::Layout) -> *mut u8 {
+        self.inner.alloc(layout)
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: std::alloc::Layout) {
+        self.inner.dealloc(ptr, layout)
     }
 }
 
