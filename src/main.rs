536c6eabb27f74183b2bbf48b963257ac6d477c0
n4to4 <n4to4k@gmail.com>
2022-04-22 20:26:26 +0900

    smallstring

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+pub mod sample;
+
+use argh::FromArgs;
+
+#[derive(FromArgs)]
+/// Small string demo
+struct Args {
+    #[argh(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(FromArgs)]
+#[argh(subcommand)]
+enum Subcommand {
+    Sample(sample::Sample),
+}
+
+impl Subcommand {
+    fn run(self) {
+        match self {
+            Subcommand::Sample(x) => x.run(),
+        }
+    }
+}
+
+fn main() {
+    argh::from_env::<Args>().subcommand.run();
+}
